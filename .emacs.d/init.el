55a761a486a5f2a082545200c163a39eb9be59e1
Garrett Cox <gcox5714@gmail.com>
2023-06-24 15:08:52 -0400

    Added emacs configuration folder.

---
+++ .emacs.d/init.el
@@ -0,0 +1,22 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+(load-theme 'timu-spacegrey t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("12db058ce4ba460e067e331a67dbb05c4406d8c0d5e4504cebc059cffae55672" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; (global-display-line-numbers-mode 1)
+;; I like the idea of line numbers but not EVERWHERE
+;; Want to figure out how to only enable them for your typical code file
+
