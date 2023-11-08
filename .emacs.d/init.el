4900e111c34bdc933b5afeddc9ff54bfb87feaff
Garrett Cox <gcox5714@gmail.com>
2023-11-08 14:32:46 -0500

    Update init.el from work environment

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,22 +1,35 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
-(load-theme 'timu-spacegrey t)
+(load-theme 'atom-one-dark t)
+
+(if (display-graphic-p)
+    (enable-theme 'atom-one-dark)
+  (disable-theme 'atom-one-dark))
+
+(tool-bar-mode -1)
+
+(set-frame-font "Cascadia Code" nil t)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("12db058ce4ba460e067e331a67dbb05c4406d8c0d5e4504cebc059cffae55672" default)))
+ '(package-selected-packages '(magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
 
 
-;; (global-display-line-numbers-mode 1)
-;; I like the idea of line numbers but not EVERyWHERE
-;; Want to figure out how to only enable them for your typical code file
+(global-display-line-numbers-mode 1)
 
+(set-window-scroll-bars (minibuffer-window) 0 'none)
