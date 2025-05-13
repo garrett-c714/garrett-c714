fc9d3884242303869d8632d2d5c7c710a0f69181
Garrett Cox <gcox5714@gmail.com>
2025-05-13 12:38:25 -0400

    Add auto-cd-ing for opening directories + (maybe) files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,25 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
+-- Automatically Change Working Dir When Opening Directories
+vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+                local arg = vim.fn.argv(0)
+                if vim.fn.isdirectory(arg) == 1 then
+                        vim.cmd('cd ' .. arg)
+                end
+        end
+})
+
+-- Locally Change Working Dir When Opening Files
+--[[
+vim.api.nvim_create_autocmd("BufEnter", {
+        callback = function()
+                vim.cmd("lcd %:p:h")
+        end
+})
+]]
+
 -- Load Lua Files
 
 -- General Settings
